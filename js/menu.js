83938008d2b77924a822ab682b5f640dbba59da2
amatjkay <amatjkay@gmail.com>
2023-05-29 23:12:22 +0300

    +

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
-// Найти все ссылки начинающиеся на #
-const anchors = document.querySelectorAll('a[href^="#"]')
+import smoothscroll from "smoothscroll-polyfill";
 
-// Цикл по всем ссылкам
-for(let anchor of anchors) {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault() // Предотвратить стандартное поведение ссылок
-    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
-    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
-    // Плавная прокрутка до элемента с id = href у ссылки
-    document.querySelector(goto).scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    })
-  })
-}
+// kick off the polyfill!
+smoothscroll.polyfill();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const anchors = document.querySelectorAll('a[href^="#"]');
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", (event) => {
+            event.preventDefault();
+            const targetId = anchor.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    }
+});
